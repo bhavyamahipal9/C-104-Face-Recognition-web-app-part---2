bdd9167fe0372db94f713ad1adc85badab8ee7c5
bhavyamahipal9 <71548514+bhavyamahipal9@users.noreply.github.com>
2022-01-18 14:13:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'png',
+    image_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function capture_image(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'">';
+    })
+}
+
+console.log('ml5.version',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EFLdn93f5/model.json',modelLoaded);
